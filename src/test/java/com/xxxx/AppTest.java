2f16a5e3324c86cc1701e3f203b555a8151048ff
lkf0986 <67904724+lkf0986@users.noreply.github.com>
2020-07-07 12:11:57 +0800

    Update AppTest.java

--- src/test/java/com/xxxx/AppTest.java
+++ src/test/java/com/xxxx/AppTest.java
@@ -19,6 +19,6 @@ public class AppTest
         System.out.println("test....");
         System.out.println("test01....");
         System.out.println("test02....");
-
+        System.out.println("test03....");
     }
 }
